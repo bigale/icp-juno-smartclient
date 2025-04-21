cc48bf60b8f4122cd1d4f74e1d1d70be8c44285d
Alex Everitt <bigale@netzero.net>
2025-04-20 20:49:48 -0400

    Fix tailwind which I don't even know if it used

--- juno.config.ts
+++ juno.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from '@junobuild/config';
 
-export default defineConfig({
+export default defineConfig(() => ({
   satellite: {
     ids: {
-      development: process.env.SATELLITE_ID_DEVELOPMENT,
-      production: process.env.SATELLITE_ID_PRODUCTION
-    },
-    source: '.next',
-    predeploy: ['npm run build']
+      development: process.env.SATELLITE_ID_DEVELOPMENT || '',
+      production: process.env.SATELLITE_ID_PRODUCTION || ''
+    }
   }
-});
+}));
