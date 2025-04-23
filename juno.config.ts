4d04239d15666df38be5c297e1d9e45d06db15de
Alex Everitt <bigale@netzero.net>
2025-04-23 10:45:01 -0400

    Deploy changes

--- juno.config.ts
+++ juno.config.ts
@@ -6,5 +6,6 @@ export default defineConfig(() => ({
       development: process.env.SATELLITE_ID_DEVELOPMENT || '',
       production: process.env.SATELLITE_ID_PRODUCTION || ''
     }
-  }
+  },
+  source: 'dist'
 }));
